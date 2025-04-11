99750ca279dd6cc8ade4d84af989be539e6a1359
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-04-11 14:59:50 +0700

    added additional argument to pgatest to choose between starting from heap or stack, removed printf %ld for bitmask since xv6 doesnt see anything with more than 64bits as anything more than 32bit integers

--- user/pgatest.c
+++ user/pgatest.c
@@ -17,32 +17,42 @@ void print_as_bin(uint64 btms)
 
 int main(int argc, char** argv)
 {
-    if(argc != 3)
+    uint64 DUMMY = 123456; // this is the dummy thing to start from
+
+    
+    if(argc != 4)
     {
-        printf("Usage: pgatest <pages_to_check> <number_of_integers_to_spawn>\n" );
+        printf("Usage: pgatest <pages_to_check> <number_of_integers_to_spawn> <\"heap\"/\"stack\">\n");
         exit(-1);
     }
     
     int spawn_int_count = atoi(argv[2]);
     int pages_num = atoi(argv[1]);
-    if(spawn_int_count <= 0 || pages_num <= 0){
-        printf("invalid argument (cannot be negative)\n");
+    if(spawn_int_count <= 0 || pages_num <= 0 || (strcmp(argv[3], "heap") && strcmp(argv[3], "stack"))){
+        printf("invalid argument\n");
         exit(-1);
     }
     
-    uint64 DUMMY = 123456; // this is the dummy thing to start from
-
+    
     int* i = malloc(spawn_int_count * sizeof(int));
     for(int it = 0; it < spawn_int_count; it += 1)
         i[it] = 0;
     uint64 res;
-    if(pgaccess(&DUMMY, pages_num, &res) == 0) {
-        //printf("%p\n", (uint64*)res);
-        printf("%ld\n", res);
-        print_as_bin(res);
+    if(strcmp(argv[3], "stack") == 0)
+    {    
+        if(pgaccess(&DUMMY, pages_num, &res) == 0)// DUMMY should be in the stack
+            print_as_bin(res);
+        else
+            printf("somethings wrong\n");
     }
-    else
-        printf("somethings wrong\n");
+    else if(strcmp(argv[3], "heap") == 0)
+    {
+        if(pgaccess((uint64 *)i, pages_num, &res) == 0)// since i is a malloc'd array, it should be in heap
+            print_as_bin(res);
+        else
+            printf("somethings wrong\n");
+    }
+
     free(i);
     return 0;
 }
